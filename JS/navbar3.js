550c755206e1c32e0d7fce2c3a70ab7c2cf61910
jaqui <jaquelin.mfarango@gmail.com>
2023-11-07 18:53:26 -0600

    Se agregó JS de categorías y se modificó navbar

--- JS/navbar3.js
+++ JS/navbar3.js
@@ -7,30 +7,31 @@ document.addEventListener('DOMContentLoaded', function () {
         var nav = document.createElement('nav');
         nav.className = 'navbar navbar-expand-md';
         nav.innerHTML = `
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li><a class="nav-link" href="./index.html">Home</a></li>
-                        <li><a class="nav-link" aria-current="page" href="./nosotras.html">Nosotros</a></li>
-                        <li><a class="nav-link" href="./contacto.html">Contacto</a></li>
-                        <li><a class="nav-link" href="./contacto.html">Categorias</a></li>
-                    </ul>
-                </div>
-                <div>  
-                    <img id="LogoPrincipal" src="./src/images/LogoSinFondo1.png"> 
-                </div>
+           <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img class="img-fluid" src="./src/images/LogoSinFondo1.png" alt="" width="300"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li><a class="nav-link" href="./index.html">Home</a></li>
+                            <li><a class="nav-link" aria-current="page" href="./nosotras.html">Nosotros</a></li>
+                            <li><a class="nav-link" href="./contacto.html">Contacto</a></li>
+                            <li><a class="nav-link" href="./contacto.html">Categorias</a></li>
+                </ul>
                 <a class="nav-link" href="./login.html">Log in</a>
                 <form class="d-flex justify-content-between" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <i class="bi bi-search"></i>
                 </form>
                 <span class="material-icons shopping">
-                    <i class="fa-solid fa-cart-shopping"></i>
+                    <i class="bi bi-cart3"></i>
                 </span>
             </div>
+        </div>
+    </nav>
         `;
         
         // Agregar el elemento nav al header
