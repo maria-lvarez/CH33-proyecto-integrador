10c4160a3ce073f79da37a8083f70ba7bf38464f
Laura Galeana <iselacastillogaleana1@gmail.com>
2023-11-21 18:36:51 -0600

    Intentando arreglando el merge conflict

--- JS/registro.js
+++ JS/registro.js
@@ -1,9 +1,8 @@
-// Mover estos let
-// let txtNombre = document.getElementById("InputNombreComp");
-// let txtCorreo = document.getElementById("InputCorreo");
-// let txtTelefono = document.getElementById("InputTelReg");
-// let txtPassword = document.getElementById("InputPasswordReg");
-// let txtConfPassword = document.getElementById("InputPasswordConf");
+let txtNombre = document.getElementById("InputNombreComp");
+let txtCorreo = document.getElementById("InputCorreo");
+let txtTelefono = document.getElementById("InputTelReg");
+let txtPassword = document.getElementById("InputPasswordReg");
+let txtConfPassword = document.getElementById("InputPasswordConf");
 
 let btnCrearCuenta = document.getElementById("btnCrearCuenta"); 
 let btnClear = document.getElementById("btnClear");
@@ -11,7 +10,6 @@ let btnClear = document.getElementById("btnClear");
 //Trear el alert
 let alertValidaciones = document.getElementById("alertValidaciones");
 
-<<<<<<< HEAD
 //Arreglo que va a almacena los elementos 
 datos = JSON.parse(localStorage.getItem("datos")) || [];
 
@@ -21,24 +19,10 @@ datos = JSON.parse(localStorage.getItem("datos")) || [];
         return true;
     }
     else {
-=======
-//Arreglo que va a almacena los elementos de mi tabla
-
-// const forms = document.getElementsByClassName('.needs-validation')
-
-datos = JSON.parse(localStorage.getItem("datos")) || [];
-
-function validarNombre(nameInput){
-
-    if (nameInput) {
-        return true
-    }else{
->>>>>>> 1e1ee0214d6f7d70f71a7ba170910e5c195ab22d
         return false;
     }
 }//ValidarNombre */
 
-<<<<<<< HEAD
 function validarCorreo(){
     let validCorreo = /^([\-\.a-zA-Z0-9]+)@([\-\.a-zA-Z0-9]+)\.([a-zA-Z]){2,7}$/;
     if(validCorreo.test(txtCorreo.value)){
@@ -65,52 +49,13 @@ function validarContrasena1(){
         return true;
     }
     else {
-=======
-function validarCorreo(emailInput){
-    if (emailInput) {
-        return true
-    }else{
         return false;
-    }
-}//validarEstado
-
-function validarPassword(passwordInput){
-
-    if (passwordInput) {
-        return true
-    }else{
-        return false;
-    }
-}//validarPassword 
-
-function validarPasswordConf(passwd, passConf){
-    console.group('validarPasswordConf');
-    console.log(passwd)
-    console.log(passConf);
-    console.groupEnd();
-    if (passwd && passwd === passConf) {
-        console.log('Is Valid');
-        console.groupEnd();
-        return true
-        
-    }else{
-        console.log('Is not Valid');
-        console.groupEnd();
->>>>>>> 1e1ee0214d6f7d70f71a7ba170910e5c195ab22d
-        return false;
-        
     }
 }//ValidarContrasena1
 
-<<<<<<< HEAD
 function validarContrasena2(){
     let validContrasena2 = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
     if(validContrasena2.test(txtConfPassword.value)){
-=======
-
-function validarTelefono(telNumber){
-    if(telNumber.length ===10){
->>>>>>> 1e1ee0214d6f7d70f71a7ba170910e5c195ab22d
         return true;
     }
     else {
@@ -121,35 +66,9 @@ function validarTelefono(telNumber){
 
 
 btnCrearCuenta.addEventListener("click", function(event){
-<<<<<<< HEAD
-=======
-    event.preventDefault();
-
-    const form = document.getElementById('formReg');
-    form.classList.add('was-validated');
-
-    // Para aca y usar const.
-    const txtNombre = document.getElementById("InputNombreComp").value;
-    const txtCorreo = document.getElementById("InputCorreo").value;
-    const txtTelefono = document.getElementById("InputTelReg").value;
-    const txtPassword = document.getElementById("InputPasswordReg").value;
-    const txtConfPassword = document.getElementById("InputPasswordConf").value;
-
-    console.log(txtNombre);
-    console.log(txtCorreo);
-    console.log(txtTelefono);
-    console.log(txtPassword);
-    console.log(txtConfPassword);
-
-    // form.classList.add('was-validated') 
-
-    let isValid = true;
-
->>>>>>> 1e1ee0214d6f7d70f71a7ba170910e5c195ab22d
     
     let isValid = true;
 
-<<<<<<< HEAD
     event.preventDefault();
     //Arreglo que va a almacena los elementos de mi tabla
     //const forms = document.getElementsByClassName('.needs-validation')
@@ -168,48 +87,29 @@ btnCrearCuenta.addEventListener("click", function(event){
         alertValidaciones.innerHTML = "El campo <strong> Nombre </strong> es requerido <br/>";
         alertValidaciones.style.display = "block";
         txtNombre.style.border = "solid thin red";
-=======
-    if(txtNombre.length < 2){ //Debo de indicar que quiero su value. Si la palabra tiene menos de 3 letras.
-        
->>>>>>> 1e1ee0214d6f7d70f71a7ba170910e5c195ab22d
         isValid = false;
     } */
 
-<<<<<<< HEAD
     if (txtNombre.value.length < 3) {
         alertValidaciones.innerHTML = "El campo <strong> Nombre </strong> es requerido <br/>";
         alertValidaciones.style.display = "block";
         txtNombre.style.border = "solid thin red";
-=======
-    if(! validarCorreo(txtCorreo)){
->>>>>>> 1e1ee0214d6f7d70f71a7ba170910e5c195ab22d
         isValid = false;
     }
 
-<<<<<<< HEAD
     if (!validarCorreo()) {
         alertValidaciones.innerHTML += "El campo <strong>Email</strong> es requerido <br/>";
         alertValidaciones.style.display = "block";
         txtCorreo.style.border = "solid thin red";
-=======
-    if(! validarPassword(txtPassword)){
->>>>>>> 1e1ee0214d6f7d70f71a7ba170910e5c195ab22d
         isValid = false;
     }
 
-<<<<<<< HEAD
     if (!validarTelefono()) {
         alertValidaciones.innerHTML += "El campo <strong>Teléfono</strong> es requerido <br/>";
         alertValidaciones.style.display = "block";
         txtTelefono.style.border = "solid thin red";
         isValid = false;
     }
-=======
-    if(! validarPasswordConf(txtPassword, txtConfPassword)){
-        isValid = false;
-        return;
-    }//If ! validarCategoria
->>>>>>> 1e1ee0214d6f7d70f71a7ba170910e5c195ab22d
 
     if (!validarContrasena1()) {
         alertValidaciones.innerHTML += "El campo <strong>Contraseña </strong> es requerido. Debe contener mínimo un número, una letra mayúscula y una minúscula, al menos 8 o más carácteres.<br/>";
@@ -218,14 +118,10 @@ btnCrearCuenta.addEventListener("click", function(event){
         isValid = false;
     }
 
-<<<<<<< HEAD
     if (!validarContrasena2()) {
         alertValidaciones.innerHTML += "El campo <strong>Confirmación de contraseña</strong> es requerido <br/>";
         alertValidaciones.style.display = "block";
         txtConfPassword.style.border = "solid thin red";
-=======
-    if(! validarTelefono(txtTelefono)){
->>>>>>> 1e1ee0214d6f7d70f71a7ba170910e5c195ab22d
         isValid = false;
     }
 
@@ -237,24 +133,8 @@ btnCrearCuenta.addEventListener("click", function(event){
             });
     }
 
-<<<<<<< HEAD
 
     if(isValid && txtConfPassword.value===txtPassword.value){
-=======
-    //JSON
-    if(isValid){ 
-        //Si es valido el nombre y la cantidad los agregará a la tabla, si no, no los agregará
-        const correo = txtCorreo;
-        const password = txtPassword;
-
-            let elemento = {
-                name : txtNombre,
-                telefono:  txtTelefono,
-                correo: txtCorreo,
-                password: txtPassword,
-                confpass: txtConfPassword
-            };
->>>>>>> 1e1ee0214d6f7d70f71a7ba170910e5c195ab22d
 
         let element = { name : txtNombre.value,
             telefono:  txtTelefono.value,
@@ -283,11 +163,6 @@ btnCrearCuenta.addEventListener("click", function(event){
 
 
 
-<<<<<<< HEAD
-=======
-    } //isValid
-
-    if(txtConfPassword.value===txtPassword.value){window.location.href = "login.html";}
->>>>>>> 1e1ee0214d6f7d70f71a7ba170910e5c195ab22d
 });//btnEnviar.addEventListener
+
 
